bc097f83f1fa4f51d60b38205a7c8ec70d625eef
nicolasances <nicolas.matteazzi@gmail.com>
2025-10-27 10:40:02 +0100

    Fixed port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { RegisterAgent } from "./dlg/catalog/PostAgent";
 import { PostTask } from "./dlg/PostTask";
 import { UpdateAgent } from "./dlg/catalog/PutAgent";
 
-const api = new TotoAPIController("gale-broker", new ControllerConfig(), { basePath: '/galebroker', port: 8081 });
+const api = new TotoAPIController("gale-broker", new ControllerConfig(), { basePath: '/galebroker' });
 
 // Endpoints related to Agent Catalog
 api.path('POST', '/catalog/agents', new RegisterAgent(), { contentType: 'application/json', noAuth: true, ignoreBasePath: false }); // Temporary, until API-key based auth is implemented.
