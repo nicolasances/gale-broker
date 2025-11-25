060dd39f037b8a1ae704fd7a7023cfd1a54a3420
nicolasances <nicolas.matteazzi@gmail.com>
2025-11-25 16:24:01 +0100

    Reformatting

--- docs/flow/Example.ts
+++ docs/flow/Example.ts
@@ -8,32 +8,32 @@ function example() {
             nodes: [
                 {
                     name: "group-section-classification",
-                    parallelize: [{agent: {}, next: null}, {agent: {}, next: null}, {agent: {}, next: null}], 
+                    parallelize: [{ agent: {}, next: null }, { agent: {}, next: null }, { agent: {}, next: null }],
                     next: {
                         branch: {
                             name: "B1.1",
                             nodes: [
                                 {
                                     name: "group-section-genealogy",
-                                    parallelize: [{agent: {}, next: null}, {agent: {}, next: null}, {agent: {}, next: null}], 
+                                    parallelize: [{ agent: {}, next: null }, { agent: {}, next: null }, { agent: {}, next: null }],
                                     next: null
-                                }, 
+                                },
                                 {
                                     name: "group-section-personalities",
-                                    parallelize: [{agent: {}, next: null}, {agent: {}, next: null}, {agent: {}, next: null}],
-                                    next: { 
-                                        agent: {}, 
-                                        next: null 
+                                    parallelize: [{ agent: {}, next: null }, { agent: {}, next: null }, { agent: {}, next: null }],
+                                    next: {
+                                        agent: {},
+                                        next: null
                                     }
                                 }
-                            ], 
+                            ],
                             next: null
                         }
                     }
-                }, 
+                },
                 {
                     name: "group-section-timeline",
-                    parallelize: [{agent: {}, next: null}, {agent: {}, next: null}, {agent: {}, next: null}], 
+                    parallelize: [{ agent: {}, next: null }, { agent: {}, next: null }, { agent: {}, next: null }],
                     next: null
                 }
             ],
@@ -47,27 +47,21 @@ function example() {
 
 function buildingComputationTree() {
 
-    const start = (): any => {};
-    const parallelize = (name: string, agents: any[]): any => {};
-    const agent = () => {};
-    const end = () => {};
-    const branch = (name: string, nodes: any[]) => {};
-    const next = () => {};
+    const start = (): any => { };
+    const parallelize = (name: string, agents: any[]): any => { };
+    const agent = () => { };
+    const end = () => { };
+    const branch = (name: string, nodes: any[]) => { };
+    const next = () => { };
 
     start().branch("B1", [
-        parallelize("group-section-classification", [
-            agent(), agent(), agent()
-        ]).next().branch("B1.1", [
-            parallelize("group-section-genealogy", [
-                agent(), agent(), agent()
+        parallelize("group-section-classification", [agent(), agent(), agent()])
+            .next().branch("B1.1", [
+                parallelize("group-section-genealogy", [agent(), agent(), agent()]),
+                parallelize("group-section-personalities", [agent(), agent(), agent()])
+                    .next().agent()
             ]),
-            parallelize("group-section-personalities", [
-                agent(), agent(), agent()
-            ]).next().agent()
-        ]), 
-        parallelize("group-section-timeline", [
-            agent(), agent(), agent()
-        ])
+        parallelize("group-section-timeline", [agent(), agent(), agent()])
     ]).end()
 
 }
