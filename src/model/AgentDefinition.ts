d39d25c8f6f71d2ab63e4955ae8155d63dcff476
nicolasances <nicolas.matteazzi@gmail.com>
2026-02-22 06:56:35 +0100

    Agent ID introduction

--- src/model/AgentDefinition.ts
+++ src/model/AgentDefinition.ts
@@ -4,19 +4,45 @@ import { TaskId } from "./AgentTask";
 
 export class AgentDefinition {
 
+    /**
+     * Unique identifier of the Agent. 
+     * Human-readable code that is programmatically used to refer to the Agent. 
+     * 
+     * For Backward compatibility reasons, if agentId is not provided, taskId will be used instead
+     */
+    agentId: string = "";    
+
+    /**
+     * Technical Id (MongoDB ObjectId) of the AgentDefinition document in the database.
+     */
     id?: string;
+    
+    /**
+     * LEGACY
+     * 
+     * The unique identifier of the type of task this Agent can execute. This is used to match the Agent with the tasks it can execute.
+     * This was there when I thought of Agents as executors of backend async tasks, not as conversational agents. 
+     * 
+     * It's still used for backward compatibility. 
+     */
+    taskId: TaskId = ""; // The unique identifier of the type of task this Agent can execute.
+
+    /**
+     * Where can the agent be reached.
+     */
+    endpoint: AgentEndpoint = new AgentEndpoint(""); // The endpoint (URL) where the Agent can be reached.
+    
     name: string = ""; // The name of the Agent.
     description: string = ""; // The description of the Agent.
-    taskId: TaskId = ""; // The unique identifier of the type of task this Agent can execute.
     inputSchema: any = {}; 
     outputSchema: any = {}; 
-    endpoint: AgentEndpoint = new AgentEndpoint(""); // The endpoint (URL) where the Agent can be reached.
 
     static fromJSON(data: any): AgentDefinition {
 
         if (!data.name || !data.taskId || !data.endpoint || !data.inputSchema || !data.outputSchema) throw new ValidationError(400, `Invalid AgentDefinition JSON: missing required fields. Received ${JSON.stringify(data)}.`);
 
         const def = new AgentDefinition();
+        def.agentId = data.agentId || data.taskId; // For backward compatibility, if agentId is not provided, use taskId as agentId
         def.name = data.name;
         def.description = data.description || "";
         def.taskId = data.taskId;
